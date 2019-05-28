460e02d90f8cfa0d78bd6b8363ea9daaad1ff376
Tim <tim@binarysolutions.io>
2019-05-28 16:40:10 -0700

    changed tim4 prescaler to 16 from 0, period to 4095

--- Src/main.c
+++ Src/main.c
@@ -329,9 +329,9 @@ static void MX_TIM4_Init(void)
   TIM_OC_InitTypeDef sConfigOC;
 
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 0;
+  htim4.Init.Prescaler = 16;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 0;
+  htim4.Init.Period = 4095;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
